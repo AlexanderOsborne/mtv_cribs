b23d0af621c6e4b57f3a182155c30ff82e45aec1
awosborn <alanturing@example.coclear>
2020-10-20 09:09:02 -0600

    Add Renter and Renter Test

---
+++ test/renter_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+
+
+class RenterTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    renter1 = Renter.new("Jessie")
+
+    assert_equal "Jessie", renter1.name
+  end
+end
